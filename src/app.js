e370c90fcfadc3ed7a2071225d28d9c7d0992f6d
rivervil16 <agustinriverovila@gmail.com>
2022-08-23 01:55:08 +0000

    Ejercicio terminado

--- src/app.js
+++ src/app.js
@@ -6,6 +6,23 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  document.querySelector("#btnGenerar").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = generarExcusa();
+  });
   console.log("Hello Rigo from the console!");
 };
+function generarExcusa() {
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+  return who[whoIndex] + " " + what[whatIndex] + " " + when[whenIndex];
+}
